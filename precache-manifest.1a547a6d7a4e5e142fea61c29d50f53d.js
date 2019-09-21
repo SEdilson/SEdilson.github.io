02bfe527a3d57c4f7faf8fce45e73b142cb38b75
SEdilson <edilson.silva00@hotmail.com>
2019-09-21 16:43:53 -0300

    Putting my website on GitHub

---
+++ precache-manifest.1a547a6d7a4e5e142fea61c29d50f53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866d9f89f4a75a7cfef8e47d020e979e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037436b61518a41a4d8c",
+    "url": "/static/css/main.e7ce1f3e.chunk.css"
+  },
+  {
+    "revision": "61c8c26cbecc4d327b28",
+    "url": "/static/js/2.8d6ee4ec.chunk.js"
+  },
+  {
+    "revision": "037436b61518a41a4d8c",
+    "url": "/static/js/main.75f4832e.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "3f075c2d417d3e4b144bd469cbba813f",
+    "url": "/static/media/eu.3f075c2d.jpg"
+  }
+]);
